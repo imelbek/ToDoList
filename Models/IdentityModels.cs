02769cfb6d6af55314f65c7bb95f6195b48bf093
Elbek Rakhimov <irakhimov98@gmail.com>
2022-11-30 18:18:16 +0500

    Add project files.

---
+++ Models/IdentityModels.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ToDoList.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            //Database.SetInitializer<ApplicationDbContext>(new CreateDatabaseIfNotExists<ApplicationDbContext>());
+        }
+
+
+        // to add a table for toDos
+        // this class dbset is part of this data access layer to be a collection of todos
+        // that are in the table in this database that is defined by app dbcontext
+        public DbSet<ToDo> ToDos { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ Models/ToDo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    // there will be a single toDo
+    public class ToDo
+    {
+        // things that define what toDo item is
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+
+        // reference to the user
+        public virtual ApplicationUser User { get; set; }
+        // ApplicationUser is class that defines a user in this framework
+    }
+}
